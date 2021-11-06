0c0c77e394a31891ace50ef8044da55283f7a4ac
Steffen Robohm <steffen.robohm@gmail.com>
2021-11-06 17:30:21 +0100

    init

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>WTC Teilnahmebestätigung</title>
+</head>
+
+<body>
+    <form action="api.php" methode="post">
+        Vorname:  <input type="text" name="vorname"><br>
+        Nachname: <input type="text" name="nachname"><br>
+        E-Mail:   <input type="text" name="email"><br>
+        Firma:    <input type="text" name="firma"><br>
+        <input type="submit">
+    </form>
+</body>
+
+</html>
